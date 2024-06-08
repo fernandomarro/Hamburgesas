b2a94ab37dfdd82b13ed702447a75195b9fc7801
fernandomarro <129783990+fernandomarro@users.noreply.github.com>
2024-06-08 11:08:24 -0600

    cambios en bd

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: 'db_hamburgesas',
-    host: 'dpg-cpfsuool5elc738j0rr0-a.oregon-postgres.render.com',
-    database: 'db_hamburgesas',
-    password: 'HrpC2qq6RoAmJkTU9gxBR15D3QwYTcXw',
+    user: 'hamburbd',
+    host: 'dpg-cpi8shuct0pc73fof380-a.oregon-postgres.render.com',
+    database: 'hamburbd',
+    password: 'FuhfB2MBCLwDVlABkfIcoDe7IIETOwC1',
     port: parseInt('5432'),
     ssl: {
         rejectUnauthorized: false  // Importante en entornos de desarrollo con SSL autofirmado, ajustar para producción
